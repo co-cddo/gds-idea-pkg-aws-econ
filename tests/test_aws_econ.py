9f1504d1ae4d739182e2e12add053b4fc7df3368
Jarek-Rolski <jaroslaw.rolski@yahoo.com>
2026-07-09 16:02:35 +0100

    add tests

--- tests/test_aws_econ.py
+++ tests/test_aws_econ.py
@@ -1,5 +1,7 @@
 import io
+import os
 
+import awswrangler as wr
 import pandas as pd
 
 from aws_econ import AwsEcon
@@ -37,3 +39,40 @@ def test_s3_read_buckets_objects(data_frame, create_mock_aws_s3_objects):
     df.to_json(buffer)
     buffer.seek(0)
     assert buffer.read() == aws.read("json.json", "prod")
+
+
+def test_s3_write_buckets_objects(data_frame, create_mock_aws_s3_objects):
+    aws = AwsEcon()
+
+    df = data_frame
+
+    aws.write("file.csv", df)
+
+    pd.testing.assert_frame_equal(
+        df, wr.s3.read_csv(f"s3://{ECON_BUCKET_NAME}/file.csv", boto3_session=aws.dev_session), check_dtype=False
+    )
+
+
+def test_s3_download_buckets_objects(tmp_path, create_mock_aws_s3_objects):
+    aws = AwsEcon()
+
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "file.csv"
+
+    aws.download("json.json", str(p), "prod")
+
+    assert ["file.csv"] == os.listdir(d)
+
+
+def test_s3_upload_buckets_objects(tmp_path, create_mock_aws_s3_objects):
+    aws = AwsEcon()
+
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "file.txt"
+    p.write_text("Example Text")
+
+    aws.upload("upload/json.json", str(p))
+
+    assert [f"s3://{ECON_BUCKET_NAME}/upload/json.json"] == wr.s3.list_objects(f"s3://{ECON_BUCKET_NAME}/upload/")
